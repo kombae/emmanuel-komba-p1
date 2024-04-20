973a6ec45379b8d62139848b45fc761b86afd826
Ekomba <emmanuelkomba22@gmail.com>
2024-04-20 17:24:05 -0500

    Project 1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.getElementById('center').innerHTML = 'Brook Lopez';
+document.getElementById("powerForward").innerHTML = "Giannis Antetokounmpo";
+document.getElementById("smallForward").innerHTML = "Khris Middleton";
+document.getElementById("shootingGuard").innerHTML = "Patrick Beverley";
+document.getElementById("pointGuard").innerHTML = "Damian Lillard";
+
+document.getElementById("salary1").innerHTML = `45,640,084`;
+document.getElementById("salary2").innerHTML = `45,640,084`;
+document.getElementById("salary3").innerHTML = `29,320,988`;
+document.getElementById("salary4").innerHTML = `25,000,000`;
+document.getElementById("salary5").innerHTML = `2,019,706`;
+
+
